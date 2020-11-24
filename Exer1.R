80ae40ff031e12f0c01f11867acbb6f9bd23750a
huulocmedvnu <tthloc.stu13@medvnu.edu.vn>
2020-11-24 23:09:58 +0700

    coding style 2

--- Exer1.R
+++ Exer1.R
@@ -1,22 +1,22 @@
-A <- array(c(1, 1, 2, 3, 5, 1, 2), dim = c(1, 7))
-
 findMaxarray <- function(Arr) {
-  m <- min(A)
-  id <- grep(m, A)
+  m <- min(Arr)
+  id <- grep(m, Arr)
   
   for(i in id) {
     L <- i
     R <- i
     
-    while(L - 1 > 0 && A[, L - 1] > m) {
+    while(L - 1 > 0 && Arr[,L - 1] > m) {
       L = L - 1
     }
     
-    while(R + 1 <= length(A) && A[, R + 1] > m) {
+    while(R + 1 <= length(Arr) && Arr[,R + 1] > m) {
       R = R + 1
     }
     
-    print(c(L, R))
+    print(sum(Arr[,L:R]))
   }
 }
+
+A <- array(c(1, 1, 2, 3, 5, 1, 2), dim = c(1, 7))
 findMaxarray(A)
